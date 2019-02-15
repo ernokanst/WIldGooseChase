d4747a80c56ac676df5f4a083c714306a2535fac
ernokanst <ernokanst@gmail.com>
2019-02-15 18:42:57 +0300

    Несколько гусей и выбор их

--- WildGooseChase/WildGooseChase.py
+++ WildGooseChase/WildGooseChase.py
@@ -1,6 +1,11 @@
-import os, pygame, random
- 
- 
+import os
+import pygame
+import random
+import sys
+from PyQt5.QtWidgets import QWidget, QApplication, QPushButton
+from PyQt5.QtWidgets import QInputDialog
+
+
 def load_image(name, colorkey=None):
     fullname = os.path.join('data', name)
     try:
@@ -11,17 +16,34 @@ def load_image(name, colorkey=None):
     image = image.convert_alpha()
     if colorkey is not None:
         if colorkey is -1:
-            colorkey = image.get_at((0, 0))
+            сolorkey = image.get_at((0, 0))
         image.set_colorkey(colorkey)
     return image
 
 
+class Dialog_Goose(QWidget):
+    def __init__(self):
+        super().__init__()
+
+    def run(self):
+        i, okBtnPressed = QInputDialog.getItem(self, "Выбор гуся",
+	                                       "Выберите гуся",
+	                                       ("goose.png", "goose-2.png",
+	                                        "goose-3.png"), 0, False)
+        return i
+
+
+goose = 'goose.png'
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    goose = Dialog_Goose()
+    goose = goose.run()
 pygame.init()
-size = width, height = 300, 300
+size = width, height = 800, 600
 screen = pygame.display.set_mode(size)
 screen.fill((255, 255, 255))
 all_sprites = pygame.sprite.Group()
-sprite_image = load_image("image.png", -1)
+sprite_image = load_image(goose)
 sprite = pygame.sprite.Sprite(all_sprites)
 sprite.image = sprite_image
 sprite.rect = sprite.image.get_rect()
@@ -30,7 +52,7 @@ sprite.rect.y = 0
 running = True
 while running:
 	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
+		if event.type == pygame.MOUSEBUTTONDOWN:
 			running = False
 	all_sprites.draw(screen)
 	pygame.display.flip()
